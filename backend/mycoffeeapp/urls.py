c387c02355c0d2c6d9b825c869583439ba0b67e6
Sharon McAleer <splumridge01@qub.ac.uk>
2025-02-10 21:53:25 +0000

    some back end tidying after undoing the refactor

--- backend/mycoffeeapp/urls.py
+++ backend/mycoffeeapp/urls.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from django.urls import (include,  # 'include' is important to link app URLs
-                         path)
-from registration.views import (ForgotPasswordView, LoginView,
-                                ResetPasswordView, UserRegistrationView)
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from django.urls import include, path
+from registration.views import (
+    ForgotPasswordView,
+    LoginView,
+    ResetPasswordView,
+    UserRegistrationView,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path(
-        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
-    ),  # Different method of authentication
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # --potentially redundant so have replaced this with the one below -- path('api/register/', include('registration.urls')),
     path("api/register/", UserRegistrationView.as_view(), name="register"),
     path("admin/", admin.site.urls),
-    path("api/register/", UserRegistrationView.as_view(), name="register"),
     path("api/login/", LoginView.as_view(), name="login"),
     path("api/forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
     path(
